b1b5502f24c4b57cfc0863fad0a479d1e751390c
jlaes <jamilaes@gmail.com>
2017-03-27 23:51:03 +0300

    Basic Loop customisations: -Zelda icon -watch bolus increment 1 and not 10

--- WatchApp Extension/Controllers/BolusInterfaceController.swift
+++ WatchApp Extension/Controllers/BolusInterfaceController.swift
@@ -94,7 +94,7 @@ final class BolusInterfaceController: WKInterfaceController, IdentifiableClass {
             }
 
             let recommendedPickerValue = pickerValueFromBolusValue(recommendedBolus)
-            pickerValue = Int(Double(recommendedPickerValue) * 0.75)
+            pickerValue = Int(Double(recommendedPickerValue) * 1)
 
             if let valueString = formatter.string(from: NSNumber(value: recommendedBolus)) {
                 recommendedValueLabel.setText(String(format: NSLocalizedString("Rec: %@ U", comment: "The label and value showing the recommended bolus"), valueString).localizedUppercase)
@@ -122,11 +122,11 @@ final class BolusInterfaceController: WKInterfaceController, IdentifiableClass {
     // MARK: - Actions
 
     @IBAction func decrement() {
-        pickerValue -= 10
+        pickerValue -= 1
     }
 
     @IBAction func increment() {
-        pickerValue += 10
+        pickerValue += 1
     }
 
     @IBAction func deliver() {
